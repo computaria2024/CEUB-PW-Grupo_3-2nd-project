11f4023e5904cc0aed0fab266c91eb4e3c660f40
Igor Rocha <igort10rocha@sempreceub.com>
2024-11-27 11:17:06 -0300

    teste de login funcionando corretamente

---
+++ codigo/javascript/auth.js
@@ -0,0 +1,24 @@
+const validCredentials = {
+    email: "usuario@ceubao.com",
+    password: "senha123"
+  };
+  
+  function validateLogin(event) {
+    event.preventDefault();
+    const emailInput = document.querySelector('input[type="email"]').value;
+    const passwordInput = document.querySelector('input[type="password"]').value;
+  
+    if (emailInput === validCredentials.email && passwordInput === validCredentials.password) {
+      // Armazena informações de login no localStorage
+      localStorage.setItem('isAuthenticated', true);
+      localStorage.setItem('userEmail', emailInput);
+  
+      alert("Login bem-sucedido!");
+      window.location.href = "home.html"; // Redireciona para home.html
+    } else {
+      alert("Email ou senha incorretos.");
+    }
+  }
+  
+  document.querySelector('form').addEventListener('submit', validateLogin);
+  
